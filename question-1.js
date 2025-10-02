de452a7117560503738bd75a89522d405beda0ac
robert-drynan <165441895+robert-drynan@users.noreply.github.com>
2025-10-02 13:07:03 -0400

    first question

--- question-1.js
+++ question-1.js
@@ -1,1 +1,16 @@
 //Robert Drynan - question 1
+
+function lowerCaseWords(mixedArray){
+    return new Promise((resolve, reject) => {
+        if(!Array.isArray(mixedArray)){
+            reject("The input was not an array, please input an array");
+        }
+        const filtered = mixedArray
+            .filter(item => typeof item === "string")
+            .map(word => word.toLowerCase());
+        resolve(filtered);
+    });
+}
+
+const mixedArray = ['PIZZA', 10, true, 25, false, 'Wings'];
+console.log(lowerCaseWords(mixedArray));
