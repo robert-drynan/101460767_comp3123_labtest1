3226a6839dc029a569d3919919ff1c6312d44230
robert-drynan <165441895+robert-drynan@users.noreply.github.com>
2025-10-02 13:19:17 -0400

    updated file system, added question 2 and callbacks

---
+++ question-2/callbacks.js
@@ -0,0 +1,21 @@
+//callbacks.js
+
+const delayedSuccess = () => {
+    setTimeout(() => {
+        let success = {'message': 'delayed success!'}
+        console.log(success);
+    }, 500)
+}
+
+const delayedException = () => {
+    setTimeout(() => {
+        try{
+            throw new Error('error: delayed exception!');
+        }catch (e){
+            console.error(e);
+        }
+    }, 500)
+}
+
+delayedSuccess()
+delayedException()
