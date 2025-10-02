f4393b95a456bab625922d25fdc2315c6c090d3d
robert-drynan <165441895+robert-drynan@users.noreply.github.com>
2025-10-02 13:49:34 -0400

    question 2

--- question-2/question-2.js
+++ question-2/question-2.js
@@ -1,2 +1,25 @@
 //question-2.js
 
+const resolvedPromise = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let success = {'message': 'delayed success!'}
+            console.log(success);
+        }, 500)
+    });
+}
+
+const rejectedPromise = () => {
+    return new Promise((resolve, reject) =>{
+        setTimeout(() => {
+            try{
+                throw new Error("{error: 'delayed exception!'}");
+            }catch (e){
+                console.error(e);
+            }
+        }, 500)
+    });
+}
+
+resolvedPromise()
+rejectedPromise()
